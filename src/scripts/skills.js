d88be4c02424a94611722f0d91a9e5b3101b3156
Atomilin <50828840+Atomilin@users.noreply.github.com>
2019-10-16 20:24:56 +0300

    add paralax

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,14 @@
 console.log('this is skills module');
+
+function parallax(event) {
+    this.querySelectorAll('.layer').forEach(layer => {
+        let speed = layer.getAttribute('data-speed');
+        layer.style.transform = `translateY(${event.clientY*speed/1000}px) translateX(${event.clientX*speed/1000}px)`
+    });
+    this.querySelectorAll('.pic_layer').forEach(layer => {
+        let speed = layer.getAttribute('data-speed');
+        layer.style.transform = `translateY(${event.clientY*speed/1000}px) translateX(${event.clientX*speed/1000}px)`
+    });
+}
+
+document.addEventListener('mousemove', parallax);
